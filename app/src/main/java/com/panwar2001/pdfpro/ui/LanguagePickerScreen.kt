113da0eed91175197fb89f5ddffc3f41ec1e7021
Ayush Panwar <ayushpanwar691@gmail.com>
2024-07-09 01:08:47 +0530

    add divider

--- app/src/main/java/com/panwar2001/pdfpro/ui/LanguagePickerScreen.kt
+++ app/src/main/java/com/panwar2001/pdfpro/ui/LanguagePickerScreen.kt
@@ -1,6 +1,5 @@
 package com.panwar2001.pdfpro.ui
 
-import com.panwar2001.pdfpro.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -9,6 +8,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.RadioButton
@@ -18,11 +18,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.panwar2001.pdfpro.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,7 +50,7 @@ fun LanguagePickerScreen(navigateUp:()->Unit,
                 val language=stringArrayResource(id = languageId)
                 Row(horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(horizontal = 20.dp)){
+                    modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.spacing_xlarge))){
                     RadioButton(selected = currentLocale == language[0],
                         enabled = currentLocale != language[0],
                         onClick = {
@@ -60,6 +61,7 @@ fun LanguagePickerScreen(navigateUp:()->Unit,
                         Text(text = "  (${language[2]})")
                     }
                 }
+                HorizontalDivider()
             }
         }
     }
