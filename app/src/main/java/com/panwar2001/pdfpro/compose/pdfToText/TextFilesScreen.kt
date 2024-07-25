a0a1171a1b90ca9d4dee0ce6f5b61d6f4d84e65a
Ayush Panwar <ayushpanwar691@gmail.com>
2024-07-25 22:12:31 +0530

    add alert dialog to delete file

--- app/src/main/java/com/panwar2001/pdfpro/compose/pdfToText/TextFilesScreen.kt
+++ app/src/main/java/com/panwar2001/pdfpro/compose/pdfToText/TextFilesScreen.kt
@@ -1,6 +1,7 @@
 package com.panwar2001.pdfpro.compose.pdfToText
 
 import android.net.Uri
+import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -17,6 +18,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -25,13 +27,19 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,12 +55,24 @@ fun TextFilesScreen(filesInfo:List<TextFileInfo>,
                     share:(Uri)->Unit,
                     viewTextFile:(Long)->Unit,
                     isLoading:Boolean,
-                    snackBarHostState: SnackbarHostState= remember {SnackbarHostState()}
+                    snackBarHostState: SnackbarHostState= remember {SnackbarHostState()},
+                    @StringRes userMessage: Int?,
+                    snackBarMessageShown:()->Unit,
+                    isError: Boolean
 ){
-        Scaffold(
-            snackbarHost = {
-                 SnackBarHost(snackBarHostState,isError = false)
-            },
+
+    var openDeleteFileDialog by remember { mutableStateOf(false) }
+    
+    userMessage?.let{ message->
+        val snackBarText = stringResource(message)
+        LaunchedEffect(Unit) {
+            snackBarHostState.showSnackbar(snackBarText, withDismissAction = true)
+            snackBarMessageShown()
+        }
+    }
+
+    Scaffold(
+            snackbarHost = {SnackBarHost(snackBarHostState,isError = isError)},
             topBar = {
             TopAppBar(
                 title = { Text(text = "Text Files Log")},
@@ -100,17 +120,47 @@ fun TextFilesScreen(filesInfo:List<TextFileInfo>,
                             contentDescription = null)
                     }
 
-                    IconButton(onClick = {deleteFile(it.id)}) {
+                    IconButton(onClick = {openDeleteFileDialog=true}) {
                         Icon(imageVector = Icons.Outlined.Delete,
                             contentDescription = null,
                             tint = Color.Red)
                     }
                 }
                 HorizontalDivider()
+                if(openDeleteFileDialog){
+                    DeleteTextFileDialog(
+                        onDismissRequest = { openDeleteFileDialog=false},
+                        onConfirmation = { deleteFile(it.id) },
+                        dialogTitle = R.string.delete_file_dialog_title,
+                        dialogText = R.string.delete_file_dialog_text,
+                        fileName = it.fileName
+                    )
+                }
             }
         }
     }
 }
+
+@Composable
+fun DeleteTextFileDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    @StringRes dialogTitle: Int,
+    @StringRes dialogText: Int,
+    fileName: String
+) {
+    AlertDialog(
+        title = {Text(text = stringResource(id = dialogTitle))},
+        text = {Text(text = stringResource(id = dialogText,fileName))},
+        onDismissRequest = {onDismissRequest()},
+        confirmButton = {
+            TextButton(onClick = {onConfirmation()}) {Text("Confirm") }
+        },
+        dismissButton = {
+            TextButton(onClick = {onDismissRequest() }) {Text("Dismiss")}
+        })
+}
+
 
 @Preview
 @Composable
@@ -120,5 +170,8 @@ fun PreviewTextFilesScreen(){
     TextFilesScreen(filesInfo = filesInfo, navigateBack = { /*TODO*/ },
         viewTextFile = {}, share = {}, deleteFile = {},
         isLoading = true,
+        userMessage = null,
+        isError = false,
+        snackBarMessageShown = {}
     )
 }
