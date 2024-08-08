96e14458e3988e1b64f744e408162aac9352bad5
Ayush Panwar <ayushpanwar691@gmail.com>
2024-08-08 20:34:08 +0530

    solve issue of previewing ads

--- app/src/main/java/com/panwar2001/pdfpro/compose/components/ADs.kt
+++ app/src/main/java/com/panwar2001/pdfpro/compose/components/ADs.kt
@@ -1,31 +1,55 @@
 package com.panwar2001.pdfpro.compose.components
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.panwar2001.pdfpro.R
 
-/**
- * [BannerAd]
-
+/** Display [BannerAd]
+ *
+ * Check if the current composition is being rendered in a preview environment.
+ * If there is preview environment ,then display a mock Row inplace of actual Ad
+ * else display the actual Ad
+ *
  * @param adUnitResID the string resource id of the unit id of banner ad
  */
 @Composable
 fun BannerAd(modifier: Modifier = Modifier,@StringRes adUnitResID:Int) {
-    val adUnitID= stringResource(id = adUnitResID)
-    AndroidView(
-        modifier = modifier.fillMaxWidth(),
-        factory = { context ->
-            AdView(context).apply {
-                setAdSize(AdSize.BANNER)
-                adUnitId = adUnitID
-                loadAd(AdRequest.Builder().build())
-            }
+    if(LocalInspectionMode.current){
+        Row(horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .background(color = Color.Gray)
+                .padding(dimensionResource(id = R.dimen.spacing_medium))
+                .fillMaxWidth()){
+              Text(text = "Ads are displayed here")
         }
-    )
+    }else {
+        val adUnitID = stringResource(id = adUnitResID)
+        AndroidView(
+            modifier = modifier.fillMaxWidth(),
+            factory = { context ->
+                AdView(context).apply {
+                    setAdSize(AdSize.BANNER)
+                    adUnitId = adUnitID
+                    loadAd(AdRequest.Builder().build())
+                }
+            }
+        )
+    }
 }
