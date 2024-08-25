884248d461712b383ccedfc70492fb7c7bce3493
Ayush Panwar <ayushpanwar691@gmail.com>
2024-08-25 15:53:44 +0530

    organize libs, remove hardcoded plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-
-
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    alias(libs.plugins.dagger.hilt.android)  apply false
+    alias(libs.plugins.devtools.ksp)  apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
