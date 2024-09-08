b76b6ea099b743bc4d9ac6d8dc6c205266266ca7
Ayush Panwar <ayushpanwar691@gmail.com>
2024-09-08 21:01:29 +0530

    remove dynamic theme

--- core/ui/src/main/java/com/panwar2001/pdfpro/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/com/panwar2001/pdfpro/core/ui/theme/Theme.kt
@@ -1,28 +1,19 @@
 package com.panwar2001.pdfpro.core.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Red,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = White10,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
 )
 
 /**
@@ -46,16 +37,9 @@ val DarkAndroidBackgroundTheme = BackgroundTheme(color = Color.Black, tonalEleva
 @Composable
 fun PDFProTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
